10f68fd45f025ba8a90096a460f56f04e186d8d8
David Hamme <dhamme@gmail.com>
2015-01-18 13:38:15 -0800

    added checkboxes and delete buttons to tasks, added event handlers for check and delete actions

--- simple-todos.js
+++ simple-todos.js
@@ -21,6 +21,15 @@ if (Meteor.isClient) {
       // equivalent to preventDefault()
       return false;
     }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function(){
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function(){
+      Tasks.remove(this._id);
+    }
   })
 }
 
