b1c5a3587870ab8dbcdca6a52779552709b65343
David Hamme <dhamme@gmail.com>
2015-01-18 23:10:50 -0800

    added public/private post functionality

--- simple-todos.js
+++ simple-todos.js
@@ -1,6 +1,8 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
+  Meteor.subscribe("tasks");
+
   Template.body.helpers({
     tasks: function() {
       if (Session.get("hideCompleted")){
@@ -42,18 +44,27 @@ if (Meteor.isClient) {
     },
     "click .delete": function(){
       Meteor.call("deleteTask", this._id);
+    },
+    "click .toggle-private": function(){
+      Meteor.call("setPrivate", this._id, ! this.private);
     }
   });
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
+
+  Template.task.helpers({
+    isOwner: function(){
+      return this.owner === Meteor.userId();
+    }
+  })
 }
 
 Meteor.methods({
   addTask: function(text){
     if(! Meteor.userId()){
-      throw new Meteor.Error("not authorized!");
+      throw new Meteor.Error("not-authorized");
     }
     Tasks.insert({
       text: text,
@@ -64,16 +75,32 @@ Meteor.methods({
   },
 
   deleteTask: function(taskId){
-    Tasks.remove(taskId);
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+      Tasks.remove(taskId);
   },
 
   setChecked: function(taskId, setChecked){
     Tasks.update(taskId, {$set: { checked: setChecked } });
+  },
+  setPrivate: function(taskId, setToPrivate){
+    var task = Tasks.findOne(taskId);
+    if (task.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
 })
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  Meteor.publish("tasks", function(){
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId }
+      ]
+    });
+  })
 }
