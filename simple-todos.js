dd8078872b63ea6b878b444cfabad1d7c758e521
David Hamme <dhamme@gmail.com>
2015-01-18 17:13:33 -0800

    added hideCompleted and incompleteCount helpers

--- simple-todos.js
+++ simple-todos.js
@@ -3,8 +3,20 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({}, { sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")){
+        return Tasks.find({checked: {$ne: true}}, { sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, { sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
+    },
+
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true }}).count();
     }
+
   });
 
   Template.body.events({
@@ -20,6 +32,10 @@ if (Meteor.isClient) {
 
       // equivalent to preventDefault()
       return false;
+    },
+
+    "change .hide-completed input": function(event){
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
